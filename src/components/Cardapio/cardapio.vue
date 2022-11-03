42b6cc10246de19abb9d4cc165f536c2b6045e0e
Victor Dev <vhlp.develop@gmail.com>
2022-11-02 23:31:05 -0400

    Overlay Qrcode chegou-> Pronto

--- src/components/Cardapio/cardapio.vue
+++ src/components/Cardapio/cardapio.vue
@@ -30,9 +30,11 @@
             >Promoções</v-btn
           >
 
-          <v-menu 
-       class="d-none d-sm-none d-md-flex d-lg-flex d-xl-flex">
-            <template v-slot:activator="{ on }" class="d-none d-sm-none d-md-flex d-lg-flex d-xl-flex">
+          <v-menu class="d-none d-sm-none d-md-flex d-lg-flex d-xl-flex">
+            <template
+              v-slot:activator="{ on }"
+              class="d-none d-sm-none d-md-flex d-lg-flex d-xl-flex"
+            >
               <v-btn
                 :loading="loginLoader"
                 text
@@ -45,9 +47,12 @@
                 <v-icon>mdi-account</v-icon>
               </v-btn>
             </template>
-            <v-list shaped width="250px" class="pa-2" >
-              <v-list-item-group style="width: 100%" class="d-row align-center justify-center">
-                <v-list-item  v-show="logged">
+            <v-list shaped width="250px" class="pa-2">
+              <v-list-item-group
+                style="width: 100%"
+                class="d-row align-center justify-center"
+              >
+                <v-list-item v-show="logged">
                   <v-list-item-title class="navSubMenu"
                     >{{ getPerfil }}
                     <v-icon left color="green"
@@ -62,21 +67,21 @@
                 </v-list-item>
                 <v-divider></v-divider>
                 <v-list-item
-          @click="toLink('qrcodesOnCardapio')"
-          class="mb-1 text-center"
-        >
-          <h1 class="navSubMenu">Meus QrCodes</h1>
-          <v-list-item-icon>
-            <v-badge
-              :content="getQrcodesSize.toString()"
-              value="2"
-              color="green"
-              overlap
-            >
-              <v-icon>mdi-qrcode</v-icon>
-            </v-badge>
-          </v-list-item-icon>
-        </v-list-item>
+                  @click="toLink('qrcodesOnCardapio')"
+                  class="mb-1 text-center"
+                >
+                  <h1 class="navSubMenu">Meus QrCodes</h1>
+                  <v-list-item-icon>
+                    <v-badge
+                      :content="getQrcodesSize.toString()"
+                      value="2"
+                      color="green"
+                      overlap
+                    >
+                      <v-icon>mdi-qrcode</v-icon>
+                    </v-badge>
+                  </v-list-item-icon>
+                </v-list-item>
                 <v-list-item @click="moveToSettings()" v-show="logged">
                   <v-list-item-icon
                     ><v-icon v-text="'mdi-cog'"></v-icon
@@ -87,7 +92,6 @@
                     ></v-list-item-title>
                   </v-list-item-content>
                 </v-list-item>
-                
 
                 <v-list-item @click="accountLogOut()" v-show="logged">
                   <v-list-item-icon>
@@ -223,7 +227,7 @@
         <v-menu>
           <template v-slot:activator="{ on }">
             <v-list-item v-on="on" class="mb-1 text-center">
-              <h1 class="navSubMenu">Meu carrinho </h1>
+              <h1 class="navSubMenu">Meu carrinho</h1>
               <v-list-item-icon>
                 <v-badge
                   :content="getCartSize.toString()"
@@ -305,13 +309,13 @@
         <v-divider />
         <v-menu>
           <template v-slot:activator="{ on }">
-            
             <v-list-item v-show="logged" v-on="on">
-              <v-list-item-title v-if="loginLoader"
-                >
-                <v-progress-circular indeterminate color="primary" ></v-progress-circular>
-                </v-list-item-title
-              >
+              <v-list-item-title v-if="loginLoader">
+                <v-progress-circular
+                  indeterminate
+                  color="primary"
+                ></v-progress-circular>
+              </v-list-item-title>
               <v-list-item-title class="navSubMenu" v-else
                 >{{ getPerfil }}
                 <v-icon left color="green"
@@ -374,11 +378,47 @@
       <bodySkeleton></bodySkeleton>
     </v-container>
     <ContentFooter ma-0 pr-0 pl-0 fluid class="mx-auto" />
+    <v-overlay :value="getPaymentCheck">
+      <v-card
+        elevation="12"
+        class="pt-6 pb-6 mr-5 ml-5"
+        style="background: linear-gradient(90deg, #FC466B 0%, #3F5EFB 100%);"
+      >
+        <div class="pyro">
+          <div class="before"></div>
+          <div class="after"></div>
+        </div>
+        <v-row no-gutters>
+          <v-col cols="12" class="d-flex align-center justify-center">
+            <p class=" text-justify font-weight-bold text-sm-h6 text-md-h5 text-lg-h6 text-xl-h5"><v-icon fab left> mdi-qrcode</v-icon>Seu QRCODE chegou!<v-icon fab right> mdi-qrcode</v-icon></p>
+          </v-col>
+          <v-col cols="12" class="mt-6 d-flex align-center justify-center">
+            <v-btn
+
+              class="white--text buttonSearch pr-12 pl-12"
+              @click="SetPaymentCheck(false)"
+            >
+              Entendi
+            </v-btn>
+          </v-col>
+          <v-col cols="12" class="mt-4 d-flex align-center justify-center">
+            <v-btn
+              text
+              class="text-decoration-underline"
+              style="color: rgb(254, 147, 140) !important;"
+              @click="moveToQrcodes()"
+            >
+              Ver meus QrCodes
+            </v-btn>
+          </v-col>
+        </v-row>
+      </v-card>
+    </v-overlay>
   </v-app>
 </template>
 
 <script>
-import { mapActions, mapGetters } from "vuex";
+import { mapActions, mapGetters, mapMutations } from "vuex";
 import topSkeleton from "./TopcardapioSkeletonLoader.vue";
 import ContentFooter from "../Dashboard/ContentFooter";
 import bodySkeleton from "./BodycardapioSkeletonLoader.vue";
@@ -393,6 +433,7 @@ export default {
   },
   props: ["id"],
   data: () => ({
+    zIndex: 0,
     snackErro: false,
     snackSucesso: false,
     loginLoader: false,
@@ -424,6 +465,7 @@ export default {
   }),
 
   methods: {
+    ...mapMutations(["SetPaymentCheck"]),
     ...mapActions([
       "LoadStore",
       "routeToCategoria",
@@ -433,6 +475,14 @@ export default {
       "MovetoCompra",
       "autoLogin",
     ]),
+    moveToQrcodes(){
+      this.SetPaymentCheck(false)
+      this.$router.push({
+        //arrumar os props
+        name: 'qrcodesOnCardapio',
+        params: this.id,
+      });
+    },
     pushToLogin() {
       this.$router.push({
         name: "entrar",
@@ -552,6 +602,7 @@ export default {
     "getCardapio",
     "getLoaded",
     "getCart",
+    "getPaymentCheck",
     "getCartSize",
     "getnewQrCode",
     "getQrcodes",
@@ -601,6 +652,148 @@ export default {
 };
 </script>
 
+<style lang="scss">
+$particles: 60;
+$width: 500;
+$height: 400;
+
+// Create the explosion...
+$box-shadow: ();
+$box-shadow2: ();
+@for $i from 0 through $particles {
+  $box-shadow: $box-shadow,
+    random($width)-$width /
+      2 +
+      px
+      random($height)-$height /
+      1.2 +
+      px
+      hsl(random(360), 100, 50);
+  $box-shadow2: $box-shadow2, 0 0 #fff;
+}
+@mixin keyframes($animationName) {
+  @-webkit-keyframes #{$animationName} {
+    @content;
+  }
+
+  @-moz-keyframes #{$animationName} {
+    @content;
+  }
+
+  @-o-keyframes #{$animationName} {
+    @content;
+  }
+
+  @-ms-keyframes #{$animationName} {
+    @content;
+  }
+
+  @keyframes #{$animationName} {
+    @content;
+  }
+}
+
+@mixin animation-delay($settings) {
+  -moz-animation-delay: $settings;
+  -webkit-animation-delay: $settings;
+  -o-animation-delay: $settings;
+  -ms-animation-delay: $settings;
+  animation-delay: $settings;
+}
+
+@mixin animation-duration($settings) {
+  -moz-animation-duration: $settings;
+  -webkit-animation-duration: $settings;
+  -o-animation-duration: $settings;
+  -ms-animation-duration: $settings;
+  animation-duration: $settings;
+}
+
+@mixin animation($settings) {
+  -moz-animation: $settings;
+  -webkit-animation: $settings;
+  -o-animation: $settings;
+  -ms-animation: $settings;
+  animation: $settings;
+}
+
+@mixin transform($settings) {
+  transform: $settings;
+  -moz-transform: $settings;
+  -webkit-transform: $settings;
+  -o-transform: $settings;
+  -ms-transform: $settings;
+}
+
+body {
+  margin: 0;
+  padding: 0;
+  background: #003366;
+  overflow: hidden;
+}
+
+.pyro > .before,
+.pyro > .after {
+  position: absolute;
+  width: 4px;
+  height: 4px;
+  border-radius: 50%;
+  box-shadow: $box-shadow2;
+  @include animation(
+    (
+      1s bang ease-out infinite backwards,
+      1s gravity ease-in infinite backwards,
+      5s position linear infinite backwards
+    )
+  );
+}
+
+.pyro > .after {
+  @include animation-delay((1.25s, 1.25s, 1.25s));
+  @include animation-duration((1.25s, 1.25s, 6.25s));
+}
+
+@include keyframes(bang) {
+  to {
+    box-shadow: $box-shadow;
+  }
+}
+
+@include keyframes(gravity) {
+  to {
+    @include transform(translateY(200px));
+    opacity: 0;
+  }
+}
+
+@include keyframes(position) {
+  0%,
+  19.9% {
+    margin-top: 10%;
+    margin-left: 40%;
+  }
+  20%,
+  39.9% {
+    margin-top: 40%;
+    margin-left: 30%;
+  }
+  40%,
+  59.9% {
+    margin-top: 20%;
+    margin-left: 70%;
+  }
+  60%,
+  79.9% {
+    margin-top: 30%;
+    margin-left: 20%;
+  }
+  80%,
+  99.9% {
+    margin-top: 30%;
+    margin-left: 80%;
+  }
+}
+</style>
 <style>
 .slide-fade-enter-active {
   transition: all 0.3s ease;
