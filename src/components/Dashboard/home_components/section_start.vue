d46f710f781e3a5a0937fb03132aa17b71cadf5c
Victor Dev <vhlp.develop@gmail.com>
2022-12-04 19:51:21 -0400

    Hotfix dashboard inicio, section_start refeita e links arrumados

--- src/components/Dashboard/home_components/section_start.vue
+++ src/components/Dashboard/home_components/section_start.vue
@@ -4,8 +4,8 @@
         <section class=" d-flex justify-center align-center mt-12">
         <div class="px-4 mx-auto mt-12 max-w-7xl px-sm-6 px-lg-8">
             <div class="max-w-2xl mx-auto text-center">
-                <h2 class="corAlta text-md-h4 text-lg-h4 text-h6 text-decoration-underline font-weight-bold">Como funciona?</h2>
-                <p class="max-w-lg mx-auto mt-4 text-center font-weight-light corMedia">Como todo cardapio por QRCODE, pegue seu celular.</p>
+                <h2 class="text-md-h4 text-lg-h4 text-h6  font-weight-bold">3 passos para fazer seu cadastro no SemFila</h2>
+                <p class="max-w-lg mx-auto mt-4 text-center font-weight-light">Tire suas dúvidas sobre como cadastrar no SemFila para vender.</p>
             </div>
 
             <div class="d-flex justify-center mt-12 mt-lg-12">
@@ -17,28 +17,28 @@
                     <v-col cols="12" md="4" lg="4" xl="4">
                         <div>
                             <div class="d-flex align-center justify-center text-center elevation-5 mx-auto rounded-circle "  style="background-color: white;width: 60px; height:60px">
-                            <span class="text-xl  font-semibold text-gray-700 " > 1 </span>
+                            <span class="text-xl  font-semibold black--text" > 1 </span>
                         </div>
-                        <h3 class="mt-6 text-xl font-semibold leading-tight text-black mt-md-10 corMedia">Ler o QRCODE</h3>
-                        <p class="mt-4 text-base text-gray-600">Utilize a camerâ do celular para leitura do QRCODE e abra o link para acessar o cardápio.</p>
+                        <h3 class="mt-6 text-xl font-semibold leading-tight text-black mt-md-10 corMedia">Informe os seus dados de contato</h3>
+                        <p class="mt-4 text-base">Complete seu cadastro. SemFila aceita MEI, mas não é possível vender apenas com CPF. Esses serão os principais canais de contato do SemFila com você.</p>
                         </div>
                         
                     </v-col>
 
                     <v-col cols="12" md="4" lg="4" xl="4">
                         <div class="d-flex align-center justify-center text-center elevation-5 mx-auto rounded-circle "  style="background-color: white;width: 60px; height:60px">
-                            <span class="text-xl font-semibold text-gray-700"> 2 </span>
+                            <span class="text-xl font-semibold black--text"> 2 </span>
                         </div>
-                        <h3 class="mt-6 text-xl font-semibold leading-tight text-black mt-md-10 corMedia">Compre no cardápio</h3>
-                        <p class="mt-4 text-base text-gray-600">Navegue pelo cardápio e compre os produtos. Para cada produto comprado será gerado um QRCODE único. </p>
+                        <h3 class="mt-6 text-xl font-semibold leading-tight text-black mt-md-10 corMedia">Aguarde a validação</h3>
+                        <p class="mt-4 text-base text-gray-600">A SemFila leva algumas horas para validar sua empresa. </p>
                     </v-col>
 
                     <v-col cols="12" md="4" lg="4" xl="4">
                         <div class="d-flex align-center justify-center text-center elevation-5 mx-auto rounded-circle "  style="background-color: white;width: 60px; height:60px">
-                            <span class="text-xl font-semibold text-gray-700"> 3 </span>
+                            <span class="text-xl font-semibold black--text"> 3 </span>
                         </div>
-                        <h3 class="mt-6 text-xl font-semibold leading-tight text-black mt-md-10 corMedia">Mostre seu QRCODE</h3>
-                        <p class="mt-4 text-base text-gray-600">Com a compra efetuada, mostre o QRCODE para um atendente do estabelecimento realizar o scaneamento. Pronto!</p>
+                        <h3 class="mt-6 text-xl font-semibold leading-tight text-black mt-md-10 corMedia">Assine o contrato</h3>
+                        <p class="mt-4 text-base text-gray-600">Informe o restante dos dados e escolha o modelo de contrato que mais lhe atrai. A partir disso está tudo pronto para usar.</p>
                     </v-col>
                 </v-row>
             </div>
@@ -67,11 +67,9 @@
     font-family: Josefin Sans;
     src: url("../../../font/JosefinSans/JosefinSans-VariableFont_wght.ttf");
 }
-.corAlta{
-    color: rgb(254,147,140) !important;
-}
+
 .corMedia{
-    color: rgb(88, 157, 108) !important;
+    color: #F24236 !important;
 }
 *{
     font-family: Josefin Sans !important;
--- src/components/Dashboard/tornar.vue
+++ src/components/Dashboard/tornar.vue
@@ -1,12 +1,17 @@
 <template>
   <v-app>
-    <v-container pa-12 fluid class="white--text backgroundTornar textComeceDefault">
+    <v-container
+      pa-12
+      fluid
+      class="white--text backgroundTornar textComeceDefault"
+    >
       <v-row justify="center">
         <v-col cols="12" md="8" sm="8" lg="6" xl="8">
           <v-subheader class="white--text">Sobre SemFila</v-subheader>
           <h1 class="mb-5 text-start">
-            <span class="cardTitle">Chega de filas</span>, cardápios digitais estáticos e gastar com
-            equipamento eletrônico caro para realizar pedidos!
+            <span class="cardTitle">Chega de filas</span>, cardápios digitais
+            estáticos e gastar com equipamento eletrônico caro para realizar
+            pedidos!
           </h1>
           <p class="text-start">
             Criar cardápios digitais de fácil uso e acesso é a nossa paixão e
@@ -18,12 +23,14 @@
     <v-container fluid pa-12 class="text-center textComeceDefault">
       <v-row justify="center">
         <v-col cols="12" md="8" sm="10" lg="8" xl="10">
-          <v-subheader class="white--text">O que é <span class="textColorDefault ml-1">SemFila</span> </v-subheader>
+          <v-subheader class="white--text"
+            >O que é <span class="textColorDefault ml-1">SemFila</span>
+          </v-subheader>
           <v-divider dark></v-divider>
           <p class="pa-5">
             É um cardápio digital de fácil acesso e uso, onde o consumidor
             realiza compras com o estabelecimento e apresenta um QrCode que
-            possui dados criptografados da Nota fiscal e do pedido realizado.
+            possui dados criptografados do pedido realizado.
           </p>
           <v-subheader class="white--text">Nosso conceito</v-subheader>
           <v-divider dark></v-divider>
@@ -42,177 +49,250 @@
         </v-col>
       </v-row>
     </v-container>
-    <v-container fluid  pa-12 class="text-center textComeceDefault d-flex align-center justify-center">
+    <v-container
+      fluid
+      pa-12
+      class="text-center textComeceDefault d-flex align-center justify-center"
+    >
       <v-row class="d-flex justify-center">
         <v-col cols="12" md="8" sm="8" lg="8" xl="8">
-          <v-subheader class="white--text text-center text-sm-start text-md-start text-lg-start text-xl-start">O que <span class="textColorDefault ml-1">entregamos?</span> </v-subheader>
+          <v-subheader
+            class="white--text text-center text-sm-start text-md-start text-lg-start text-xl-start"
+            >O que <span class="textColorDefault ml-1">entregamos?</span>
+          </v-subheader>
           <v-row class="d-flex justify-center">
             <v-col cols="6" sm="3" md="4" lg="2" xl="2" class="d-flex">
               <v-card class="pa-2 flex-grow-1 cardPartners" width="120">
-            <v-row no-gutters>
-              <v-col cols="12" lg="7" class="d-flex justify-end">
-                
-                  <v-icon small color="red">mdi-check-decagram</v-icon>
-                
-              </v-col>
-              <v-col cols="12" class="d-flex justify-start mt-n5">
-                <v-row class="mt-4">
-                  <v-col cols="12" class="d-flex align-center justify-center">
-                    <v-avatar icon size="60" tile class="rounded-xl">
-                      <v-icon x-large color="blue">
-                        mdi-lock
-                      </v-icon>
-                    </v-avatar>
+                <v-row no-gutters>
+                  <v-col cols="12" lg="7" class="d-flex justify-end">
+                    <v-icon small color="red">mdi-check-decagram</v-icon>
                   </v-col>
-                  <v-col cols="12" class="">
-                    <p class="text-center">
-                      <span class="font-weight-bold white--text"
-                        >Segurança</span
+                  <v-col cols="12" class="d-flex justify-start mt-n5">
+                    <v-row class="mt-4">
+                      <v-col
+                        cols="12"
+                        class="d-flex align-center justify-center"
                       >
-                    </p>
-                    <p class="grey--text text-center mt-n4">na sua compra</p></v-col
-                  >
+                        <v-avatar icon size="60" tile class="rounded-xl">
+                          <v-icon x-large color="blue"> mdi-lock </v-icon>
+                        </v-avatar>
+                      </v-col>
+                      <v-col cols="12" class="">
+                        <p class="text-center">
+                          <span class="font-weight-bold white--text"
+                            >Segurança</span
+                          >
+                        </p>
+                        <p class="grey--text text-center mt-n4">
+                          na sua compra
+                        </p></v-col
+                      >
+                    </v-row>
+                  </v-col>
                 </v-row>
-              </v-col>
-            </v-row>
-          </v-card>
+              </v-card>
             </v-col>
             <v-col cols="6" sm="3" md="4" lg="2" xl="2" class="d-flex">
               <v-card class="pa-2 flex-grow-1 cardPartners" width="120">
-            <v-row no-gutters>
-              <v-col cols="12" lg="7" class="d-flex justify-end" >
-                
-                  <v-icon small color="red">mdi-check-decagram</v-icon>
-                
-              </v-col>
-              <v-col cols="12" class="d-flex justify-start mt-n5">
-                <v-row class="mt-4">
-                  <v-col cols="12" class="d-flex align-center justify-center">
-                    <v-avatar icon size="60" tile class="rounded-xl">
-                      <v-icon x-large color="secondary">
-                        mdi-package-variant-closed-check
-                      </v-icon>
-                    </v-avatar>
+                <v-row no-gutters>
+                  <v-col cols="12" lg="7" class="d-flex justify-end">
+                    <v-icon small color="red">mdi-check-decagram</v-icon>
                   </v-col>
-                  <v-col cols="12" class="">
-                    <p class="text-center">
-                      <span class="font-weight-bold white--text"
-                        >Logística</span
+                  <v-col cols="12" class="d-flex justify-start mt-n5">
+                    <v-row class="mt-4">
+                      <v-col
+                        cols="12"
+                        class="d-flex align-center justify-center"
                       >
-                    </p>
-                    <p class="grey--text text-center mt-n4">para estoque da empresa</p></v-col
-                  >
+                        <v-avatar icon size="60" tile class="rounded-xl">
+                          <v-icon x-large color="secondary">
+                            mdi-package-variant-closed-check
+                          </v-icon>
+                        </v-avatar>
+                      </v-col>
+                      <v-col cols="12" class="">
+                        <p class="text-center">
+                          <span class="font-weight-bold white--text"
+                            >Logística</span
+                          >
+                        </p>
+                        <p class="grey--text text-center mt-n4">
+                          para estoque da empresa
+                        </p></v-col
+                      >
+                    </v-row>
+                  </v-col>
                 </v-row>
-              </v-col>
-            </v-row>
-          </v-card>
+              </v-card>
             </v-col>
             <v-col cols="6" sm="3" md="4" lg="2" xl="2" class="d-flex">
               <v-card class="pa-2 flex-grow-1 cardPartners" width="120">
-            <v-row no-gutters>
-              <v-col cols="12" lg="7" class="d-flex justify-end">
-                
-                  <v-icon small color="red">mdi-check-decagram</v-icon>
-                
-              </v-col>
-              <v-col cols="12" class="d-flex justify-start mt-n5">
-                <v-row class="mt-4">
-                  <v-col cols="12" class="d-flex align-center justify-center">
-                    <v-avatar icon size="60" tile class="rounded-xl">
-                      <v-icon x-large color="green">
-                        mdi-trending-up
-                      </v-icon>
-                    </v-avatar>
+                <v-row no-gutters>
+                  <v-col cols="12" lg="7" class="d-flex justify-end">
+                    <v-icon small color="red">mdi-check-decagram</v-icon>
                   </v-col>
-                  <v-col cols="12" class="">
-                    <p class="text-center">
-                      <span class="font-weight-bold white--text"
-                        >Aumento</span
+                  <v-col cols="12" class="d-flex justify-start mt-n5">
+                    <v-row class="mt-4">
+                      <v-col
+                        cols="12"
+                        class="d-flex align-center justify-center"
                       >
-                    </p>
-                    <p class="grey--text text-center mt-n4">geral nas vendas</p></v-col
-                  >
+                        <v-avatar icon size="60" tile class="rounded-xl">
+                          <v-icon x-large color="green">
+                            mdi-trending-up
+                          </v-icon>
+                        </v-avatar>
+                      </v-col>
+                      <v-col cols="12" class="">
+                        <p class="text-center">
+                          <span class="font-weight-bold white--text"
+                            >Aumento</span
+                          >
+                        </p>
+                        <p class="grey--text text-center mt-n4">
+                          geral nas vendas
+                        </p></v-col
+                      >
+                    </v-row>
+                  </v-col>
                 </v-row>
-              </v-col>
-            </v-row>
-          </v-card>
+              </v-card>
             </v-col>
             <v-col cols="6" sm="3" md="4" lg="2" xl="2" class="d-flex">
               <v-card class="pa-2 flex-grow-1 cardPartners" width="120">
-            <v-row no-gutters>
-              <v-col cols="12" lg="7" class="d-flex justify-end">
-                
-                  <v-icon small color="red">mdi-check-decagram</v-icon>
-                
-              </v-col>
-              <v-col cols="12" class="d-flex justify-start mt-n5">
-                <v-row class="mt-4">
-                  <v-col cols="12" class="d-flex align-center justify-center">
-                    <v-avatar icon size="60" tile class="rounded-xl">
-                      <v-icon x-large color="orange">
-                        mdi-cash-check
-                      </v-icon>
-                    </v-avatar>
+                <v-row no-gutters>
+                  <v-col cols="12" lg="7" class="d-flex justify-end">
+                    <v-icon small color="red">mdi-check-decagram</v-icon>
                   </v-col>
-                  <v-col cols="12" class="">
-                    <p class="text-center">
-                      <span class="font-weight-bold white--text"
-                        >Gratuito</span
+                  <v-col cols="12" class="d-flex justify-start mt-n5">
+                    <v-row class="mt-4">
+                      <v-col
+                        cols="12"
+                        class="d-flex align-center justify-center"
                       >
-                    </p>
-                    <p class="grey--text text-center mt-n4">para iniciar seu negócio</p></v-col
-                  >
+                        <v-avatar icon size="60" tile class="rounded-xl">
+                          <v-icon x-large color="orange">
+                            mdi-cash-check
+                          </v-icon>
+                        </v-avatar>
+                      </v-col>
+                      <v-col cols="12" class="">
+                        <p class="text-center">
+                          <span class="font-weight-bold white--text"
+                            >Gratuito</span
+                          >
+                        </p>
+                        <p class="grey--text text-center mt-n4">
+                          para iniciar seu negócio
+                        </p></v-col
+                      >
+                    </v-row>
+                  </v-col>
                 </v-row>
-              </v-col>
-            </v-row>
-          </v-card>
+              </v-card>
             </v-col>
             <v-col cols="6" sm="3" md="4" lg="2" xl="2" class="d-flex">
               <v-card class="pa-2 flex-grow-1 cardPartners" width="120">
-            <v-row no-gutters>
-              <v-col cols="12" lg="7" class="d-flex justify-end">
-                
-                  <v-icon small color="red">mdi-check-decagram</v-icon>
-                
-              </v-col>
-              <v-col cols="12" class="d-flex justify-start mt-n5">
-                <v-row class="mt-4">
-                  <v-col cols="12" class="d-flex align-center justify-center">
-                    <v-avatar icon size="60" tile class="rounded-xl">
-                      <v-icon x-large color="primary">
-                        mdi-sitemap
-                      </v-icon>
-                    </v-avatar>
+                <v-row no-gutters>
+                  <v-col cols="12" lg="7" class="d-flex justify-end">
+                    <v-icon small color="red">mdi-check-decagram</v-icon>
                   </v-col>
-                  <v-col cols="12" class="">
-                    <p class="text-center">
-                      <span class="font-weight-bold white--text"
-                        >Simplicidade</span
+                  <v-col cols="12" class="d-flex justify-start mt-n5">
+                    <v-row class="mt-4">
+                      <v-col
+                        cols="12"
+                        class="d-flex align-center justify-center"
                       >
-                    </p>
-                    <p class="grey--text text-center mt-n4">na hora de usar</p></v-col
-                  >
+                        <v-avatar icon size="60" tile class="rounded-xl">
+                          <v-icon x-large color="primary"> mdi-sitemap </v-icon>
+                        </v-avatar>
+                      </v-col>
+                      <v-col cols="12" class="">
+                        <p class="text-center">
+                          <span class="font-weight-bold white--text"
+                            >Simplicidade</span
+                          >
+                        </p>
+                        <p class="grey--text text-center mt-n4">
+                          na hora de usar
+                        </p></v-col
+                      >
+                    </v-row>
+                  </v-col>
                 </v-row>
-              </v-col>
-            </v-row>
-          </v-card>
+              </v-card>
             </v-col>
           </v-row>
         </v-col>
-        
       </v-row>
     </v-container>
+    <v-container fluid class="text-center textComeceDefault my-10 mb-16">
+      <v-row justify="center" class="mb-10">
+        <v-col cols="12" md="8" sm="10" lg="8" xl="10">
+          <h1 class="mt-10 mb-10">
+            Conheça nosso plano <span class="textColorDefault"> SemFila</span>
+          </h1>
+          <v-row justify="center">
+            <v-col cols="12" md="6" sm="6" lg="6" xl="6">
+              <v-card>
+                <v-row class="pa-2 d-flex align-center justify-center">
+                  <v-col cols="12" class="d-flex align-center justify-center">
+                    <v-icon color="green" class="mr-2">mdi-check</v-icon
+                    ><span> Seu cardapio no SemFila</span>
+                  </v-col>
+                  <v-col cols="12" class="d-flex align-center justify-center">
+                    <v-icon color="green" class="mr-2">mdi-check</v-icon
+                    ><span> Painel administrativo</span>
+                  </v-col>
+                  <v-col cols="12" class="d-flex align-center justify-center">
+                    <v-icon color="green" class="mr-2">mdi-check</v-icon
+                    ><span> Total controle dos preços</span>
+                  </v-col>
+                  <v-col cols="12" class="d-flex align-center justify-center">
+                    <span class="font-weight-bold mr-2">5%</span>
+                    <span>Comissão sobre pedidos</span>
+                  </v-col>
+                  <v-col cols="12" class="d-flex align-center justify-center">
+                    <span class="font-weight-bold mr-2">1%</span>
+                    <span>Taxa para retirada semanal</span>
+                  </v-col>
+                  <v-col cols="12" class="d-flex align-center justify-center">
+                    <span class="font-weight-bold mr-2">0%</span>
+                    <span>Taxa para retirada mensal</span>
+                  </v-col>
+                </v-row>
+              </v-card>
+            </v-col>
+          </v-row>
+        </v-col>
+      </v-row>
+    </v-container>
+    <section_startVue></section_startVue>
     <v-container fluid class="text-center textComeceDefault my-10 mb-16">
       <v-divider dark></v-divider>
       <v-row justify="center" class="mb-10">
         <v-col cols="12" md="8" sm="10" lg="8" xl="10">
-          <h1 class="mt-10 mb-10">Qualquer um pode ser <span class="textColorDefault"> SemFila</span></h1>
+          <h1 class="mt-10 mb-10">
+            Qualquer um pode ser <span class="textColorDefault"> SemFila</span>
+          </h1>
           <v-row justify="center">
             <v-col cols="12" md="6" sm="6" lg="6" xl="6">
-                <p class="text-start pa-5 pa-sm-1 pa-md-1 pa-lg-1 pa-xl-1">Comece agora, totalmente gratuito. Com o nosso painel administrativo você tem acesso a todo nosso trabalho.</p>
+              <p class="text-start pa-5 pa-sm-1 pa-md-1 pa-lg-1 pa-xl-1">
+                Comece agora, totalmente gratuito. Com o nosso painel
+                administrativo você tem acesso a todo nosso trabalho.
+              </p>
             </v-col>
-            <v-col cols="12" md="6" sm="6" lg="6" xl="6" class="d-flex justify-center align-center">
-                <v-btn rounded outlined class="buttonSearch">Me tornar um SemFila</v-btn>
+            <v-col
+              cols="12"
+              md="6"
+              sm="6"
+              lg="6"
+              xl="6"
+              class="d-flex justify-center align-center"
+            >
+              <v-btn rounded outlined class="buttonSearch"
+                >Me tornar um SemFila</v-btn
+              >
             </v-col>
           </v-row>
         </v-col>
@@ -221,10 +301,15 @@
   </v-app>
 </template>
 <script>
-export default {};
+import section_startVue from './home_components/section_start.vue';
+export default {
+  components:{
+    section_startVue
+  }
+};
 </script>
 <style>
- .cardPartners {
+.cardPartners {
   background: rgba(16, 12, 8, 1) !important;
   border-color: #e8e8e7 !important;
   border-radius: 10px !important;
@@ -237,15 +322,14 @@ export default {};
   overflow-x: hidden !important;
   height: 100px !important;
 }
-.textHeader{
+.textHeader {
   font-size: 1.8rem;
 }
-.cardColorPartner{
-  background: #F24236 !important;
+.cardColorPartner {
+  background: #f24236 !important;
   border-color: #e8e8e7 !important;
   border-radius: 5px !important;
   border-style: solid !important;
-  
 }
 @import "./home.module.css";
 </style>
